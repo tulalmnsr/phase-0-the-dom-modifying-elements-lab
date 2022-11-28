898a4cd7316316103a14422c8761e8c2c2a3eb35
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-11-28 12:44:25 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+
+var element = document.createElement('div');
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2'));
